e9b2d000b8b649102d7092e5a0dcf72460c79795
ZhangYaoyv <2196878189@qq.com>
2019-12-19 09:21:24 +0800

    下載element-ui

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+// import Elementui from 'element-ui'
+import Elementui from 'elementui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(Elementui)
 Vue.config.productionTip = false
 
 new Vue({
