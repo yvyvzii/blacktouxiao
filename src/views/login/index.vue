d4dc4b4812424b84b5a7b6286fa0f94e134ae000
ZhangYaoyv <2196878189@qq.com>
2019-12-19 11:47:21 +0800

    靜態登錄頁

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,18 +4,23 @@
       <div class="tu">
         <img src="../../assets/img/logo_index.png" alt="">
       </div>
+      <!-- 輸入框 -->
       <el-form>
+        <!-- 手機號 -->
          <el-form-item>
            <el-input  placeholder="請輸入手機號">
            </el-input>
          </el-form-item>
+         <!-- 驗證碼 -->
          <el-form-item>
            <el-input style="width:65%" placeholder="請輸入驗證碼">
            </el-input>
            <el-button style="float:right" plain>發送驗證碼</el-button>
+           <!-- 複選框 -->
        <el-checkbox style="margin-top: 20px;">
          我已閲讀并同意
        </el-checkbox>
+       <!-- 登錄按鈕 -->
        <el-form-item>
          <el-button type="primary" style="width:100%;margin-top: 10px;" >登錄</el-button>
        </el-form-item>
