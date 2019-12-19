cfb77796791944bf62627f0808d35e7b428673f9
ZhangYaoyv <2196878189@qq.com>
2019-12-19 11:44:40 +0800

    靜態登錄頁

---
+++ src/views/home/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>主頁</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/login/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="login">
+    <el-card class="login-card">
+      <div class="tu">
+        <img src="../../assets/img/logo_index.png" alt="">
+      </div>
+      <el-form>
+         <el-form-item>
+           <el-input  placeholder="請輸入手機號">
+           </el-input>
+         </el-form-item>
+         <el-form-item>
+           <el-input style="width:65%" placeholder="請輸入驗證碼">
+           </el-input>
+           <el-button style="float:right" plain>發送驗證碼</el-button>
+       <el-checkbox style="margin-top: 20px;">
+         我已閲讀并同意
+       </el-checkbox>
+       <el-form-item>
+         <el-button type="primary" style="width:100%;margin-top: 10px;" >登錄</el-button>
+       </el-form-item>
+         </el-form-item>
+       </el-form>
+    </el-card>
+  </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='less' scoped>
+  .login{
+    background-image: url('../../assets/img/gugong.jpeg') ;
+    height: 100vh;
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .login-card{
+      width: 450px;
+      height: 330px;
+      .tu{
+        text-align: center;
+        margin-bottom: 25px;
+        img{
+          height: 42px;
+        }
+
+      }
+
+    }
+  }
+</style>
