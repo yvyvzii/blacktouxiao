8322dfddb08b0a40fcc75ab20236812c86eef65b
ZhangYaoyv <2196878189@qq.com>
2019-12-24 10:17:09 +0800

    左右側導航

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,31 @@
 <template>
-  <div>主頁</div>
+  <el-container>
+
+    <!-- 左側 -->
+    <el-aside style="height:100vh;background-color:#323745;width:230px">
+      <layout> </layout>
+    </el-aside>
+    <!-- 右側 -->
+    <el-container>
+
+      <el-header>
+        <!-- 頭部 -->
+      <layout-header></layout-header>
+      </el-header>
+
+      <el-main></el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
+import layoutaside from '../../components/home/layout'
+import layoutheader from '../../components/home/layout-header'
 export default {
-
+  components: {
+    'layout': layoutaside,
+    'layout-header': layoutheader
+  }
 }
 </script>
 
