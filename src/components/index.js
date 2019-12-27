294bcff40b56bcff594765712dad19f8f1b5c85f
ZhangYaoyv <2196878189@qq.com>
2019-12-27 20:16:59 +0800

    麵包屑組件

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,11 @@
 import layoutaside from './home/layout'
 import layoutheader from './home/layout-header'
-
+import breadcrumb from './common/bread'
 export default {
   install: function (Vue) {
     Vue.component('layout', layoutaside)// 注冊一個全局組件
     Vue.component('layout-header', layoutheader)// 注冊一個全局組件
+    Vue.component('bread', breadcrumb)
   }
 
 }
